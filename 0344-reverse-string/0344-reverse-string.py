98476fdc015afd5947538d6fbed80f83c6c4054c
ynghan <119781387+ynghan@users.noreply.github.com>
2024-04-25 09:58:20 +0900

    Time: 178 ms (17.36%), Space: 20.8 MB (93.50%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        tmp = 0
-        for i in range(len(s)//2): # 0 ~ len(s) - 1 + 1
-            tmp = s[i]
-            s[i] = s[len(s) -i-1]
-            s[len(s) -i-1] = tmp
+                                    # if len(s) == 10:
+        for i in range(len(s)//2):  # 0 ~ 5 까지 반복문을 돌린다.
+            tmp = 0                     # 임시 저장 변수를 둔다.   
+            tmp = s[i]              # s[0]
+            s[i] = s[len(s)-i-1]    # s[0] = s[9]
+            s[len(s)-i-1] = tmp     # s[9] = tmp
         
-        return s
             
         
